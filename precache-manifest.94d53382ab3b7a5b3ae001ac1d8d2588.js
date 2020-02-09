eded47ad48b07474e55b16450dd645dab179644d
farman <farman@techcetra.com>
2020-02-09 17:46:55 +0530

    Updates

---
+++ precache-manifest.94d53382ab3b7a5b3ae001ac1d8d2588.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35a5303d1660f99cb08fef4d9e6723d4",
+    "url": "/alarmclock/index.html"
+  },
+  {
+    "revision": "d6a679c87b04d6efbe23",
+    "url": "/alarmclock/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "0bea50ece1dadb137b59",
+    "url": "/alarmclock/static/css/main.cebc02d2.chunk.css"
+  },
+  {
+    "revision": "d6a679c87b04d6efbe23",
+    "url": "/alarmclock/static/js/2.14ea380e.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/alarmclock/static/js/2.14ea380e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0bea50ece1dadb137b59",
+    "url": "/alarmclock/static/js/main.c111517d.chunk.js"
+  },
+  {
+    "revision": "1cc0faf0617910dc0099",
+    "url": "/alarmclock/static/js/runtime-main.eac77109.js"
+  },
+  {
+    "revision": "238acd50a6dd935336d61e14235449bf",
+    "url": "/alarmclock/static/media/beep.238acd50.mp3"
+  }
+]);
